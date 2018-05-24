2b3413dc5b55a7f3d88cdfba2055d340b65c7f4c
Marta Beveridge <marta@beveridge.uk>
2018-05-24 14:02:36 +0100

    removed spare space from runner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,8 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'da8tfqoa8eu457', host: 'ec2-23-21-129-50.compute-1.amazonaws.com
-', port: 5432, user: 'prlazcuifmqbsf', password: 'e1658fafbcada95776029e82f96c62b0969679de5bbbd8a71a236350f24e97f4'})
+      db = PG.connect({ dbname: 'da8tfqoa8eu457', host: 'ec2-23-21-129-50.compute-1.amazonaws.com', port: 5432, user: 'prlazcuifmqbsf', password: 'e1658fafbcada95776029e82f96c62b0969679de5bbbd8a71a236350f24e97f4'})
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
